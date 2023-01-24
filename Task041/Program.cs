997f58c07c756b1cb392b32d0aaf6f843547aadd
Sant <stosoleg@gmail.com>
2023-01-24 13:31:29 +0300

    Task041 done

---
+++ Task041/Program.cs
@@ -0,0 +1,24 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+int[] enterArray()
+{
+    Console.WriteLine("Введите количество элементов массива");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int count = 0;
+foreach (var item in enterArray())
+{
+    Console.Write($"{item} ");
+    if (item > 0) count += 1;
+
+}
+Console.WriteLine();
+Console.WriteLine($"Колличество положительных чисел = {count}");
